34dc53c9f766ad2d135016fea33bd9ef49181e61
TomsaJ <julian.tomsa@gmail.com>
2023-12-11 15:18:16 +0100

    add Maven

--- Notenverwaltung/src/de/fh/swf/se/s2/Pflichtmodul.java
+++ Notenverwaltung/src/de/fh/swf/se/s2/Pflichtmodul.java
@@ -18,6 +18,12 @@ public class Pflichtmodul {
     private double[] mNote = {1.0, 1.3, 1.7, 2.0, 2.3, 2.7, 3.0 ,3.3, 3.7, 4.0, 5.0};
 
 
+	/**
+	 * @param modulName
+	 * @param creditpoints
+	 * @param beschreibung
+	 * @param semester
+	 */
     public Pflichtmodul(String modulName, int creditpoints, String beschreibung, int semester) {
 		this.pModulName = modulName;
 		this.pCreditpoints = creditpoints;
